5e96d2fac00ee803d53db5e1f520c95c6b99742f
josiasdev <josiasmartins098@gmail.com>
2024-10-18 12:10:08 -0300

    adicionei uma função main para chamar a função partidas rankeadas

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-function partidasRankeadas(qtdVitorias, qtdDerrotas){
+function partidasRankeadas(qtdVitorias, qtdDerrotas) {
     let resultadoRankeada = qtdVitorias - qtdDerrotas;
     let nivel;
-    if (qtdVitorias < 10){
+    if (qtdVitorias < 10) {
         nivel = "Ferro";
     }
-    else if (qtdVitorias >= 11 && qtdVitorias <= 20){
+    else if (qtdVitorias >= 11 && qtdVitorias <= 20) {
         nivel = "Bronze";
     }
-    else if (qtdVitorias >= 21 && qtdVitorias <= 50){
+    else if (qtdVitorias >= 21 && qtdVitorias <= 50) {
         nivel = "Prata";
     }
-    else if (qtdVitorias >= 51 && qtdVitorias <= 80){
+    else if (qtdVitorias >= 51 && qtdVitorias <= 80) {
         nivel = "Ouro";
     }
-    else if (qtdVitorias >= 81 && qtdVitorias <= 90){
+    else if (qtdVitorias >= 81 && qtdVitorias <= 90) {
         nivel = "Diamante";
     }
-    else if (qtdVitorias >= 91 && qtdVitorias <= 100){
+    else if (qtdVitorias >= 91 && qtdVitorias <= 100) {
         nivel = "Lendário";
     }
-    else if (qtdVitorias >= 101){
+    else if (qtdVitorias >= 101) {
         nivel = "Imortal";
     }
 
-console.log("O Herói tem saldo de "+ resultadoRankeada, "está no nivel de " + nivel);
+    console.log("O Herói tem saldo de " + resultadoRankeada, "está no nivel de " + nivel);
 }
 
-partidasRankeadas(150, 20)
+function main() {
+    partidasRankeadas(150, 20)
+}
+
+main();
